dbc5eb5b5501cfac4d0a0608475f84e20622d84e
Mary Kohn <marymkohn@gmail.com>
2023-06-09 23:09:04 -0400

    add node.js express server

--- client/js/main.js
+++ client/js/main.js
@@ -1,29 +1,33 @@
-import * as dotenv from 'dotenv'
-dotenv.config();
-const apiKey = process.env.API_KEY;
-
 // calls getLocation when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     getLocation();
 });
 
 function getLocation() {
-    if (navigator.geolocation) { // requests the users location
-        navigator.geolocation.getCurrentPosition(showPosition); // if yes, use showPosition to get lat and lon
+    const getLocationButton = document.getElementById('loc'); //HTML Element
+    if (navigator.geolocation) {
+        getLocationButton.textContent = "Checking your location...";
+        navigator.geolocation.getCurrentPosition(success, fail);
     } else {
-        console.log("Geolocation is not supported by this browser."); // if no, it shows this message in the console
+        getLocationButton.textContent = "Unsupported browser";
+        console.log("Geolocation is not supported by this browser.");
     }
 }
 
-function showPosition(position) { // takes the 'position' object as an argument
-    const lat = position.coords.latitude; // extract lat from geoLocation
-    const lon = position.coords.longitude; // extract lon from geoLocation
-    getWeather(lat, lon); // calls getWeather using the lat and lon as parameters
+function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    getWeather(lat, lon);
+}
+
+function fail(message) {
+    const getLocationButton = document.getElementById('loc');
+    getLocationButton.textContent = "Location Unavailable";
+    console.log(message.code);
 }
 
 async function getWeather(lat, lon) {
-    // Call your server here instead of the OpenWeatherMap API directly
-    const apiUrl = `/getWeather?lat=${lat}&lon=${lon}&units=imperial`;
+    const apiUrl = `/getWeather?latitude=${lat}&longitude=${lon}&units=imperial`;
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
@@ -33,18 +37,14 @@ async function getWeather(lat, lon) {
     }
 }
 
-
-// this function extracts data that we want from the API response
 function displayWeather(data) {
     const city = data.name;
     const weather = data.weather[0].description;
     const temperature = Math.round(data.main.temp);
-
-    // the data is displayed in the HTML class weatherContainer
     const weatherContainer = document.querySelector("#weather-container");
     weatherContainer.innerHTML = `
-    <h2>${city}</h2>
-    <p>${weather}</p>
-    <p>${temperature}°F</p>
-  `;
+        <h2>${city}</h2>
+        <p>${weather}</p>
+        <p>${temperature}°F</p>
+    `;
 }
