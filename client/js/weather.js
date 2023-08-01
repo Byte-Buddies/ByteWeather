d2e46ad7a9404377fa56a866dd15c61d11787d0b
Mary Kohn <marymkohn@gmail.com>
2023-08-01 10:14:13 -0400

    add https:// to fetching URL

--- client/js/weather.js
+++ client/js/weather.js
@@ -1,6 +1,6 @@
 async function fetchWeatherData(params) {
 		try {
-				const response = await fetch(`http://byteweather.byte-buddies.com/weather?${params}`);
+				const response = await fetch(`https://byteweather.byte-buddies.com/weather?${params}`);
 				if (response.ok) {
 						return await response.json();
 				} else {
